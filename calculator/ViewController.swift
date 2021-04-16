2dfb11945c9ab51c79fcc42014e19a29fdf62159
sserpilozdemir <serpil.ozdemir@windowslive.com>
2021-04-17 00:21:15 +0200

    firstApp
    main

---
+++ calculator/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by user196457 on 4/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var result: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func add(_ sender: Any) {
+        
+        if let firstGiven  = Int(firstNumber.text!) {
+            if let secondGiven = Int(secondNumber.text!) {
+                
+                let sonuc = firstGiven + secondGiven
+                result.text = String(sonuc)
+            }
+        }
+            
+    }
+    
+    @IBAction func substraction(_ sender: Any) {
+        
+        if let firstGiven  = Int(firstNumber.text!) {
+            if let secondGiven = Int(secondNumber.text!) {
+                let sonuc = firstGiven - secondGiven
+                result.text = String(sonuc)
+            }
+        }
+    }
+    @IBAction func carpma(_ sender: Any) {
+        
+        if let firstGiven  = Int(firstNumber.text!) {
+            if let secondGiven = Int(secondNumber.text!) {
+                let sonuc = firstGiven * secondGiven
+                result.text = String(sonuc)
+            }
+        }
+    }
+        
+    @IBAction func bolmee(_ sender: Any) {
+        
+        if let firstGiven  = Int(firstNumber.text!) {
+            if let secondGiven = Int(secondNumber.text!) {
+                let sonuc = firstGiven / secondGiven
+                result.text = String(sonuc)
+            }
+        }
+    }
+
+}
